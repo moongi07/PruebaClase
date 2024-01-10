53495be1a38fb25b3621de239d8d86aa022324b0
Andrea <andreamartinezmoreno@hotmail.com>
2024-01-10 10:40:02 +0100

    añadir prueba java

---
+++ PRUEBAGITHUB/src/main/java/com/paclase/pruebagithub/PRUEBAGITHUB.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.paclase.pruebagithub;
+
+/**
+ *
+ * @author Andrea
+ */
+public class PRUEBAGITHUB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("adios");
+           
+    }
+}
